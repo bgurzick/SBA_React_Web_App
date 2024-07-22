52ef74ad1106e3324d0418d6713276730c04c9e2
bgurzick <bgurzick@gmail.com>
2024-07-21 23:41:36 -0700

    commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,19 +9,19 @@ function HomePage() {
       <Link to="/year/2020">
         <div className="year-card">
           <h2>2020</h2>
-          <p>Click to reveal 20 movies from 2020</p>
+          <p>20 movies from 2020</p>
         </div>
       </Link>
       <Link to="/year/2010">
         <div className="year-card">
           <h2>2010</h2>
-          <p>Click to reveal 10 movies from 2010</p>
+          <p>10 movies from 2010</p>
         </div>
       </Link>
       <Link to="/year/1990">
         <div className="year-card">
           <h2>1990</h2>
-          <p>Click to reveal 90 movies from 1990</p>
+          <p>90 movies from 1990</p>
         </div>
       </Link>
     </div>
